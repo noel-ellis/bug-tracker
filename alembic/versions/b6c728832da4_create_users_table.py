554caee2b80279e381aea1d0579bf1faeff14df3
noel-ellis <noel_ellis@proton.me>
2022-11-02 16:34:58 +0100

    fixed alembic issue

--- alembic/versions/b6c728832da4_create_users_table.py
+++ alembic/versions/b6c728832da4_create_users_table.py
@@ -17,19 +17,8 @@ depends_on = None
 
 
 def upgrade() -> None:
-    op.create_table('users',
-                    sa.Column('id', sa.Integer(), nullable=False, primary_key=True),
-                    sa.Column('username', sa.String(), nullable=False, unique=True),
-                    sa.Column('email', sa.String, nullable=False, unique=True),
-                    sa.Column('password', sa.String, nullable=False),
-                    sa.Column('name', sa.String),
-                    sa.Column('surname', sa.String),
-                    sa.Column('access', sa.String, server_default='user', nullable = False),
-                    sa.Column('created_at', sa.TIMESTAMP(timezone=True), nullable=False, server_default=sa.text('now()'))
-                    )
     pass
 
 
 def downgrade() -> None:
-    op.drop_table('users')
     pass
